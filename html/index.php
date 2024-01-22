20540f69bdeb416209108f787a1b884a4d368661
SajanaFathima <sajana@titumlk.onmicrosoft.com>
2024-01-22 12:16:02 +0530

    css.php

--- html/index.php
+++ html/index.php
@@ -2,43 +2,8 @@
 <html lang="en">
   <!-- Mirrored from dreamsrent.dreamstechnologies.com/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Jan 2024 07:33:31 GMT -->
   <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=0"
-    />
-    <title>City Taxi</title>
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-
-    <!-- Fontawesome CSS -->
-    <link
-      rel="stylesheet"
-      href="assets/plugins/fontawesome/css/fontawesome.min.css"
-    />
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
-
-    <!-- Select2 CSS -->
-    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css" />
-
-    <!-- Datepicker CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css" />
-
-    <!-- Aos CSS -->
-    <link rel="stylesheet" href="assets/plugins/aos/aos.css" />
-
-    <!-- Fearther CSS -->
-    <link rel="stylesheet" href="assets/css/feather.css" />
-
-    <!-- Owl carousel CSS -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="assets/css/style.css" />
+  <?php include 'css.php'; ?>
+    
   </head>
   <body>
     <div class="main-wrapper">
