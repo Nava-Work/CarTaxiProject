9c8f5392d5a373d216b1b6619140ec5c61c37744
SajanaFathima <sajana@titumlk.onmicrosoft.com>
2024-01-22 12:17:36 +0530

    script  php

--- html/index.php
+++ html/index.php
@@ -77,37 +77,8 @@
     <!-- scrollToTop end -->
 
     <!-- jQuery -->
-    <script
-      data-cfasync="false"
-      src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
-    ></script>
-    <script src="assets/js/jquery-3.7.1.min.js"></script>
-
-    <!-- Bootstrap Core JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-    <!-- counterup JS -->
-    <script src="assets/js/jquery.waypoints.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-
-    <!-- Select2 JS -->
-    <script src="assets/plugins/select2/js/select2.min.js"></script>
-
-    <!-- Aos -->
-    <script src="assets/plugins/aos/aos.js"></script>
-
-    <!-- Top JS -->
-    <script src="assets/js/backToTop.js"></script>
-
-    <!-- Datepicker Core JS -->
-    <script src="assets/plugins/moment/moment.min.js"></script>
-    <script src="assets/js/bootstrap-datetimepicker.min.js"></script>
-
-    <!-- Owl Carousel JS -->
-    <script src="assets/js/owl.carousel.min.js"></script>
-
-    <!-- Custom JS -->
-    <script src="assets/js/script.js"></script>
+    <?php include 'script.php'; ?>
+   
   </body>
 
   <!-- Mirrored from dreamsrent.dreamstechnologies.com/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Jan 2024 07:34:04 GMT -->
