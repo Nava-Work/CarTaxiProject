65a24283dae2ac59a0fa77fb4cf512b345fc480d
tkarshi <tkarshi2000@gmail.com>
2024-01-25 14:47:49 +0530

    resolve

--- html/register.php
+++ html/register.php
@@ -1,3 +1,77 @@
+<?php
+session_start(); // Start the session
+require 'database-config.php';
+
+	if(isset($_POST["signup"]))
+	{
+	$na = $_POST["name"];
+    $una = $_POST["username"];
+    $pass= $_POST["password"];
+    $email = $_POST["email"];
+    $phonenum = $_POST["phone_number"];
+
+	$sql = "insert into passenger (name, username, password, email, phone_number) values('$na','$una','$pass','$email','$phonenum') ";
+		if(mysqli_query($conn, $sql))
+		{
+			echo 
+			"
+			<script>
+			alert('Registration successful');
+			document.location.href = 'login.php';
+			</script>
+			"
+			;
+			// $_SESSION['registration_success'] = "Registration successful.";
+			// header("Location: login.php");
+        	// exit();
+		}
+		else {
+			echo "Unable to insert data: " . mysqli_error($conn);
+		}
+	}
+?>
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["signup"]))
+{
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'sajanaesoft@gmail.com';      //my gmail account
+    $mail->Password = 'pnuh sbvo akve aqgg';    //copy from that app password
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('sajanaesoft@gmail.com'); //my gmail
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+    $mail->Subject = "Confirmation Email";
+    $mail->Body = "Username: " . $_POST["username"] . "<br>Password: " . $_POST["password"];
+
+    $mail->send();
+
+    echo
+    "
+    <script>
+    alert('sent successfully');
+    document.location.href = 'login.php';
+    </script>
+    ";
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
