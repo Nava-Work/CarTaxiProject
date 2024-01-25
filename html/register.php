ba319ce4a551e1bcbf53c89ef82250fab556f411
SajanaFathima <sajana@titumlk.onmicrosoft.com>
2024-01-25 14:29:26 +0530

    send mail using php mailer this is for passenger

--- html/register.php
+++ html/register.php
@@ -31,6 +31,46 @@ require 'database-config.php';
 	}
 ?>
 
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["signup"]))
+{
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'sajanaesoft@gmail.com';      //my gmail account
+    $mail->Password = 'pnuh sbvo akve aqgg';    //copy from that app password
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('sajanaesoft@gmail.com'); //my gmail
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+    $mail->Subject = "Confirmation Email";
+    $mail->Body = "Username: " . $_POST["username"] . "<br>Password: " . $_POST["password"];
+
+    $mail->send();
+
+    echo
+    "
+    <script>
+    alert('sent successfully');
+    document.location.href = 'login.php';
+    </script>
+    ";
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
