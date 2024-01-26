e283706d92f6276551a31ce495c76fe5f7fa119b
SajanaFathima <sajana@titumlk.onmicrosoft.com>
2024-01-26 20:27:36 +0530

    admin manage driver

--- html/admin-manage-driver.php
+++ html/admin-manage-driver.php
@@ -1,9 +1,9 @@
 <?php
 require 'database-config.php';
 
-// Execute the database query to retrieve all passengers
-$sql = "SELECT * FROM passenger;";
-$output = mysqli_query($conn, $sql);
+// Execute the database query to retrieve all drivers
+// $sql = "SELECT * FROM drivers;";
+// $output = mysqli_query($conn, $sql);
 ?>
 
 
@@ -39,7 +39,7 @@ $output = mysqli_query($conn, $sql);
                 </form> -->
  
                 
-                    <table id="passengerTable" class="table table-striped table-bordered table-hover">
+                    <table id="driverTable" class="table table-striped table-bordered table-hover">
                         <thead class="thead-dark">
                             <tr>
                                 <th>ID</th>
@@ -47,35 +47,36 @@ $output = mysqli_query($conn, $sql);
                                 <th>Username</th>
                                 <th>Password</th>
                                 <th>Email</th>
+                                <th>NIC</th>
                                 <th>Phone Number</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
-                            // if(isset($_POST["searchAll"]))
-                            // {
-                                $sql = "SELECT * FROM passenger;";
+                          
+                                $sql = "SELECT * FROM drivers;";
                                 $output = mysqli_query($conn, $sql);
                                 while($r = mysqli_fetch_array($output))
                                 {
                                     echo "<tr>";
-                                    echo "<td>" . $r['pass_id'] . "</td>";
+                                    echo "<td>" . $r['driver_id'] . "</td>";
                                     echo "<td>" . $r['name'] . "</td>";
                                     echo "<td>" . $r['username'] . "</td>";
                                     echo "<td>" . $r['password'] . "</td>";
                                     echo "<td>" . $r['email'] . "</td>";
+                                    echo "<td>" . $r['NIC'] . "</td>";
                                     echo "<td>" . $r['phone_number'] . "</td>";
                                     echo "<td>";
-                                    echo "<a href='edit_passenger.php?pass_id={$r['pass_id']}' class='btn btn-warning btn-sm'>Update</a>";
+                                    echo "<a href='edit_passenger.php?driver_id={$r['driver_id']}' class='btn btn-warning btn-sm'>Update</a>";
                                     //echo "<button class='btn btn-warning btn-sm'>Update</button>";
                                     echo "<form method='post' action='delete.php' style='display:inline;'>";
-                                    echo "<input type='hidden'  name='pass_id' value='{$r['pass_id']}'>";
+                                    echo "<input type='hidden'  name='driver_id' value='{$r['driver_id']}'>";
                                     echo "<button type='submit' class='btn btn-danger btn-sm' name='delete'>Delete</button>";
                                     echo "</td>";
                                     echo "</tr>";
                                 }
-                            //}
+                           
                             ?>
                         </tbody>
 
@@ -91,7 +92,7 @@ $output = mysqli_query($conn, $sql);
         <!-- DataTables Initialization Script -->
         <script>
             $(document).ready(function () {
-                $('#passengerTable').DataTable();
+                $('#driverTable').DataTable();
             });
         </script>
     </body>
--- html/driver-register.php
+++ html/driver-register.php
@@ -8,15 +8,15 @@ require 'database-config.php';
     $una = $_POST["username"];
     $pass= $_POST["password"];
     $email = $_POST["email"];
-    $phonenum = $_POST["phone_number"];
 	$nic = $_POST["NIC"];
+    $phonenum = $_POST["phone_number"];
 
 
 	 // Hash the password before storing it
 	 $hashed_password = password_hash($pass, PASSWORD_DEFAULT);
 
 
-	$sql = "insert into drivers (name, username, password, email, NIC, phone_number) values('$na','$una','$hashed_password','$email',$nic,'$phonenum') ";
+	$sql = "insert into drivers (name, username, password, email, NIC, phone_number) values('$na','$una','$hashed_password','$email','$nic','$phonenum') ";
 		if(mysqli_query($conn, $sql))
 		{
 			echo 
