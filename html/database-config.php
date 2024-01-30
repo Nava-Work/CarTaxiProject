c3fef498de397df3b5469514f6e27d6f67632658
SajanaFathima <sajana@titumlk.onmicrosoft.com>
2024-01-30 11:30:43 +0530

    db

--- html/database-config.php
+++ html/database-config.php
@@ -2,7 +2,7 @@
 
 <?php
 
-$conn = mysqli_connect("localhost",  "root",  "",  "citytaxi");  //"14_cartaxi"-> database name based on ur db u can change in ur local code
+$conn = mysqli_connect("localhost",  "root",  "",  "taxi");  //"14_cartaxi"-> database name based on ur db u can change in ur local code
 
 if (!$conn) {
     die("Connection Error: " . mysqli_connect_error());
