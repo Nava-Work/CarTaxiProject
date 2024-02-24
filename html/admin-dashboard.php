64e7882cd8df91d523d3b6d4d83dce8757301b60
navarasan <navarasan84@gmail.com>
2024-02-24 11:43:31 +0530

    admin dashboard modified

--- html/admin-dashboard.php
+++ html/admin-dashboard.php
@@ -2,48 +2,9 @@
     <div class="main-wrapper">
 
     <?php include 'admin-header.php'; ?>
+    <?php include 'admin-banner.php'; ?>
     <?php include 'admin-sidebar.php'; ?>
 
-    		<!-- Car Grid View -->
-		<section class="section car-listing">
-			<div class="container">
-				<div class="row">
-					<div class="col-xl-3 col-lg-4 col-sm-12 col-12 theiaStickySidebar">
-				 	</div>
-					<div class="col-xl-9 col-lg-8 col-sm-12 col-12">
-						<div class="row">
-                            <div class="listview-car">
-                                <div class="card">
-                                    <div class="blog-widget d-flex">
-                                        <div class="blog-img">
-                                            <a href="passenger-vehicle-list.php">
-                                                <!-- <img src="assets/img/car-list-1.jpg" class="img-fluid" alt="blog-img"> -->
-                                            </a>														    
-                                        </div>
-                                        <div class="bloglist-content w-20">
-                                           <div class="card-body">
-                                                <div class="blog-list-head d-flex">
-                                                </div>	
-                                                <div class="blog-list-head list-head-bottom d-flex">
-                                                    <div class="blog-list-title">
-                                                    </div>
-                                                    <div class="listing-button">
-                                                        <a href="admin-dashboard.php" class="btn btn-order"><span><i class="feather-calendar me-2"></i></span>Reserve Now</a>
-                                                    </div>
-                                                </div>	
-                                            </div>					 
-                                        </div>			 
-                                    </div> 
-                                </div>
-							</div>
-
-                           
-                        </div>
-					</div>
-				</div>		
-		  	</div>	
-		</section>	
-		<!-- /Car Grid View -->		
 
     <?php include 'script.php'; ?>
     </body>
